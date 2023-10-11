9883698f4cbd90dfbe62a45c180180b64a66e8a4
Tongyu Zhou <tongyuzhou98@gmail.com>
2023-10-11 16:38:04 -0400

    click and drag now working

--- client/PhoneMouse/PhoneMouse/ViewController.swift
+++ client/PhoneMouse/PhoneMouse/ViewController.swift
@@ -6,38 +6,133 @@
 //
 
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet var trackingView: UIView!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    
+    let motionManager = CMMotionManager()
+    let panMode = "touch"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        startAccelerometerUpdates()
         // Do any additional setup after loading the view.
-    }
-
-
-    @IBAction func leftMouseClick(_ sender: Any) {
-        print("left clicked")
-        sendMessage(message: "left click")
-    }
-    
-    @IBAction func rightMouseClick(_ sender: Any) {
-        print("right clicked")
-        sendMessage(message: "right click")
-    }
-    
-    @IBAction func handlePan(_ recognizer: UIPanGestureRecognizer) {
-        guard let recognizerView = recognizer.view else {
-            return
-        }
+        let panGestureRecognizerLeftBtn = UIPanGestureRecognizer(target: self, action: #selector(handlePanLeftMouse(_:)))
+        panGestureRecognizerLeftBtn.delegate = self
+        leftButton.addGestureRecognizer(panGestureRecognizerLeftBtn)
         
-        let translation = recognizer.translation(in: view)
-        print(translation.x, translation.y)
-        sendMessage(message: "move: \(translation.x), \(translation.y)")
-        recognizer.setTranslation(.zero, in: view)
+        let panGestureRecognizerRightBtn = UIPanGestureRecognizer(target: self, action: #selector(handlePanRightMouse(_:)))
+        panGestureRecognizerRightBtn.delegate = self
+        rightButton.addGestureRecognizer(panGestureRecognizerRightBtn)
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        panGestureRecognizer.delegate = self
+        trackingView.addGestureRecognizer(panGestureRecognizer)
+        
+        panGestureRecognizer.require(toFail: panGestureRecognizerLeftBtn)
+        panGestureRecognizer.require(toFail: panGestureRecognizerRightBtn)
+    }
+    
+    // on screen touch events
+    
+    @IBAction func leftMouseDown(_ sender: Any) {
+        print("left click down")
+        sendMessage(message: "left down")
+    }
+    
+    @IBAction func leftMouseUp(_ sender: Any) {
+        print("left click up")
+        sendMessage(message: "left up")
+    }
+    
+    @IBAction func rightMouseDown(_ sender: Any) {
+        print("right click down")
+        sendMessage(message: "right down")
+    }
+    
+    @IBAction func rightMouseUp(_ sender: Any) {
+        print("right click up")
+        sendMessage(message: "right up")
+    }
+
+    @IBAction func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+            case .changed:
+                let translation = recognizer.translation(in: trackingView)
+                print("Finger moved by (\(translation.x), \(translation.y))")
+                sendMessage(message: "move: \(translation.x), \(translation.y)")
+                recognizer.setTranslation(.zero, in: trackingView)
+            default:
+                break
+        }
+    }
+    
+    @IBAction func handlePanLeftMouse(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+            case .changed:
+                let translation = recognizer.translation(in: trackingView)
+                print("Finger moved by (\(translation.x), \(translation.y))")
+                sendMessage(message: "move: \(translation.x), \(translation.y)")
+                recognizer.setTranslation(.zero, in: trackingView)
+            case .ended, .cancelled, .failed:
+                print("left click up")
+                sendMessage(message: "left up")
+            default:
+                break
+        }
+    }
+    
+    @IBAction func handlePanRightMouse(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+            case .changed:
+                let translation = recognizer.translation(in: trackingView)
+                print("Finger moved by (\(translation.x), \(translation.y))")
+                sendMessage(message: "move: \(translation.x), \(translation.y)")
+                recognizer.setTranslation(.zero, in: trackingView)
+            case .ended, .cancelled, .failed:
+                print("right click up")
+                sendMessage(message: "right up")
+            default:
+                break
+        }
+    }
+    
+    
+    // physical motion events
+    
+    func startAccelerometerUpdates() {
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.1  // Set your desired update interval in seconds
+            motionManager.startAccelerometerUpdates(to: .main) { accelerometerData, error in
+                guard let data = accelerometerData else { return }
+                
+                // Adjust the threshold value as needed
+                let threshold: Double = 0.2
+                
+                if abs(data.acceleration.x) > threshold || abs(data.acceleration.y) > threshold || abs(data.acceleration.z) > threshold {
+                    self.sendMessage(message: "phone moved")
+                }
+            }
+        }
+    }
+
+    func stopAccelerometerUpdates() {
+        if motionManager.isAccelerometerActive {
+            motionManager.stopAccelerometerUpdates()
+        }
     }
     
     func sendMessage(message: String) {
-        guard let url = URL(string: "http://192.168.86.111:5000/click") else { return }
+        // mac at brown
+//        guard let url = URL(string: "http://10.39.14.93:5000/touch") else { return }
+//        // mac
+        guard let url = URL(string: "http://192.168.86.106:5000/touch") else { return }
+        // windows
+//        guard let url = URL(string: "http://192.168.86.111:5000/touch") else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -46,9 +141,10 @@ class ViewController: UIViewController {
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 print("Error: \(error)")
-            } else if let data = data {
-                print("Response: \(String(data: data, encoding: .utf8) ?? "")")
             }
+//            else if let data = data {
+//                print("Response: \(String(data: data, encoding: .utf8) ?? "")")
+//            }
         }
 
         task.resume()
--- server/server.py
+++ server/server.py
@@ -5,26 +5,40 @@ mouse = Controller()
 
 app = Flask(__name__)
 
+mousePressed = False
+
 @app.route('/')
 def about():
     return 'Load success'
 
-@app.route('/click', methods=['POST'])
-def receive_click():
+@app.route('/touch', methods=['POST'])
+def receive_touch():
     data = request.get_data(as_text=True)
-    print(f"Received click with data: {data}")
-    # Process the click here
-    if data == "left click":
-        mouse.click(Button.left, 1)
-    elif data == "right click":
-        mouse.click(Button.right, 1)
+    print(f"Received touch event with data: {data}")
+    # Process the touch event here
+    if data == "left down":
+        mouse.press(Button.left)
+        return 'Left down'
+    elif data == "left up":
+        mouse.release(Button.left)
+        return 'Left up'
+    elif data == "right down":
+        mouse.press(Button.right)
+        return 'Right down'
+    elif data == "right up":
+        mouse.release(Button.right)
+        return 'Right up'
     elif data.startswith("move:"):
         move_instructions = data.split(": ")[1].split(", ")
         x = float(move_instructions[0])
         y = float(move_instructions[1])
         mouse.move(x, y)
-
-    return 'Click received successfully'
+        return 'Moved'
 
 if __name__ == '__main__':
-    app.run(host='192.168.86.111', port=5000, debug=True, threaded=False)
+    # mac
+    app.run(host='192.168.86.106', port=5000, debug=True, threaded=False)
+    # mac at brown
+    # app.run(host='10.39.14.93', port=5000, debug=True, threaded=False)
+    # windows machine
+    # app.run(host='192.168.86.111', port=5000, debug=True, threaded=False)
