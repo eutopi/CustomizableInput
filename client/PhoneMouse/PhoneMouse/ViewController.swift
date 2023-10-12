04a070a19336d6b61ad3946b4e0eed487f7a9509
Tongyu Zhou <tongyuzhou98@gmail.com>
2023-10-11 22:38:53 -0400

    scroll working, adding UI designs for traditional vs. trackpad modes

--- client/PhoneMouse/PhoneMouse/ViewController.swift
+++ client/PhoneMouse/PhoneMouse/ViewController.swift
@@ -8,19 +8,48 @@
 import UIKit
 import CoreMotion
 
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet var trackingView: UIView!
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet var mouseModeToggleLabel: UILabel!
+    
+    @IBOutlet var leftButtonPhysical: UIButton!
+    @IBOutlet var rightButtonPhysical: UIButton!
+    @IBOutlet var traditionalScrollbar: UIImageView!
+    @IBOutlet var traditionalBG1: UIImageView!
+    @IBOutlet var traditionalBG2: UIImageView!
     
     let motionManager = CMMotionManager()
-    let panMode = "touch"
+    var mouseMode = "traditional"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 //        startAccelerometerUpdates()
         // Do any additional setup after loading the view.
+        initializeGestures()
+        initializeButtons()
+    }
+    
+    func initializeGestures() {
         let panGestureRecognizerLeftBtn = UIPanGestureRecognizer(target: self, action: #selector(handlePanLeftMouse(_:)))
         panGestureRecognizerLeftBtn.delegate = self
         leftButton.addGestureRecognizer(panGestureRecognizerLeftBtn)
@@ -35,6 +64,44 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         
         panGestureRecognizer.require(toFail: panGestureRecognizerLeftBtn)
         panGestureRecognizer.require(toFail: panGestureRecognizerRightBtn)
+        
+        let panGestureRecognizerScrollbar = UIPanGestureRecognizer(target: self, action: #selector(handleScrollbarTouch(_:)))
+        panGestureRecognizerScrollbar.delegate = self
+        traditionalScrollbar.addGestureRecognizer(panGestureRecognizerScrollbar)
+    }
+    
+    func initializeButtons() {
+        leftButton.backgroundColor = UIColor(hex: "#2047C4")
+        rightButton.backgroundColor = UIColor(hex: "#2047C4")
+        leftButton.layer.cornerRadius = 5
+        rightButton.layer.cornerRadius = 5
+        leftButton.isHidden = true
+        rightButton.isHidden = true
+        
+        let maskLayerLeft = CAShapeLayer()
+        maskLayerLeft.path = UIBezierPath(
+            roundedRect: leftButtonPhysical.bounds,
+            byRoundingCorners: [.topRight, .bottomRight],
+            cornerRadii: CGSize(width: 10, height: 10)
+        ).cgPath
+        
+        let maskLayerRight = CAShapeLayer()
+        maskLayerRight.path = UIBezierPath(
+            roundedRect: rightButtonPhysical.bounds,
+            byRoundingCorners: [.topLeft, .bottomLeft],
+            cornerRadii: CGSize(width: 10, height: 10)
+        ).cgPath
+        
+        
+        leftButtonPhysical.backgroundColor = .white
+        leftButtonPhysical.layer.cornerRadius = 35
+        leftButtonPhysical.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+        leftButtonPhysical.layer.mask = maskLayerLeft
+        
+        rightButtonPhysical.backgroundColor = .white
+        rightButtonPhysical.layer.cornerRadius = 35
+        rightButtonPhysical.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        rightButtonPhysical.layer.mask = maskLayerRight
     }
     
     // on screen touch events
@@ -59,7 +126,21 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         sendMessage(message: "right up")
     }
 
+    @IBAction func handleScrollbarTouch(_ recognizer: UIPanGestureRecognizer) {
+        if mouseMode != "traditional" { return }
+        switch recognizer.state {
+            case .changed:
+                let translation = recognizer.translation(in: trackingView)
+                print("Finger moved by (\(translation.x), \(translation.y))")
+                sendMessage(message: "scroll: \(translation.y)")
+                recognizer.setTranslation(.zero, in: trackingView)
+            default:
+                break
+        }
+    }
+    
     @IBAction func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        if mouseMode != "trackpad" { return }
         let numberOfTouches = recognizer.numberOfTouches
         switch recognizer.state {
             case .changed:
@@ -78,6 +159,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     @IBAction func handlePanLeftMouse(_ recognizer: UIPanGestureRecognizer) {
+        if mouseMode != "trackpad" { return }
         switch recognizer.state {
             case .changed:
                 let translation = recognizer.translation(in: trackingView)
@@ -93,6 +175,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     @IBAction func handlePanRightMouse(_ recognizer: UIPanGestureRecognizer) {
+        if mouseMode != "trackpad" { return }
         switch recognizer.state {
             case .changed:
                 let translation = recognizer.translation(in: trackingView)
@@ -104,6 +187,36 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
                 sendMessage(message: "right up")
             default:
                 break
+        }
+    }
+    
+    
+    @IBAction func handleSwitchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            
+            leftButton.isHidden = true
+            rightButton.isHidden = true
+            leftButtonPhysical.isHidden = false
+            rightButtonPhysical.isHidden = false
+            traditionalScrollbar.isHidden = false
+            traditionalBG1.isHidden = false
+            traditionalBG2.isHidden = false
+            mouseMode = "traditional"
+            mouseModeToggleLabel.text = "Traditional"
+            mouseModeToggleLabel.textColor = .white
+        }
+        else {
+            leftButton.isHidden = false
+            rightButton.isHidden = false
+            leftButtonPhysical.isHidden = true
+            rightButtonPhysical.isHidden = true
+            traditionalScrollbar.isHidden = true
+            traditionalBG1.isHidden = true
+            traditionalBG2.isHidden = true
+            trackingView.backgroundColor = .white
+            mouseMode = "trackpad"
+            mouseModeToggleLabel.text = "Trackpad"
+            mouseModeToggleLabel.textColor = .black
         }
     }
     
