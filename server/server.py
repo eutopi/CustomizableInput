b0c2ac7ad635e4b77ac6c31350bf4b0eca51358c
Tongyu Zhou <tongyuzhou98@gmail.com>
2024-01-23 17:00:06 -0500

    code that reads keyboard input working (but only on windows -- mac seg faults

--- server/server.py
+++ server/server.py
@@ -1,26 +1,64 @@
 from flask import Flask, request
 from pynput.mouse import Button, Controller
+from pynput import keyboard
 from modules import *
 import socket
 
 
 mouse = Controller()
 sliderModule = SliderModule()
+recorded_inputs = []
 
 app = Flask(__name__)
-
 mousePressed = False
+
+def on_press(key):
+    global recorded_inputs
+    print(key)
+    if already_pressed(recorded_inputs, (key, 'down'), (key, 'up')):
+        print('already pressed')
+        pass
+    else:
+        print('not already pressed')
+        recorded_inputs.append((key, 'down'))
+
+def on_release(key):
+    global recorded_inputs
+    print(key)
+    recorded_inputs.append((key, 'up'))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        print(recorded_inputs)
+        return False
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
 
 @app.route('/')
 def about():
     return 'Load success'
 
-@app.route('/start-recordining', methods=['POST'])
+@app.route('/start-recording', methods=['POST'])
 def start_recording():
-    
+    # Collect events until released
+    listener.start()
+
+@app.route('/stop-recording', methods=['POST'])
+def stop_recording():
+    listener.stop()
+    print(recorded_inputs)
+
+@app.route('/upload-recording', methods=['POST'])
+def upload_recording():
+    pass
+
+@app.route('/press-button', methods=['POST'])
+def press_button():
+    pass
 
 @app.route('/change-slider', methods=['POST'])
-def receive_slider_change():
+def change_slider():
     data = request.get_data(as_text=True)
     data = data.split("###")
     sliderModule.call_function(data[0], int(float(data[1])*100))
@@ -57,6 +95,23 @@ def receive_touch():
         y = int(float(scroll_instructions))
         mouse.scroll(0, y)
         return 'Scrolled'
+    
+def already_pressed(l, a, b):
+    # Check if b is not in l
+    if a not in l:
+        return False
+    if b not in l:
+        # Check if there is only incidence of a in l
+        if l.count(a) == len(l):
+            return True
+        else:
+            return False
+    else:
+        # Check if the most recent incidence of a in l appears after b
+        last_index_a = max([i for i, x in enumerate(l) if x == a], default=-1)
+        last_index_b = l.index(b) if b in l else -1
+
+        return last_index_a > last_index_b
 
 if __name__ == '__main__':
     ip_address = socket.gethostbyname(socket.gethostname())
--- server/test.py
+++ server/test.py
@@ -2,32 +2,48 @@
 
 from pynput import keyboard
 
-running = True
+recorded_inputs = []
+
+def already_pressed(l, a, b):
+    # Check if b is not in l
+    if a not in l:
+        return False
+    if b not in l:
+        # Check if there is only incidence of a in l
+        if l.count(a) == len(l):
+            return True
+        else:
+            return False
+    else:
+        # Check if the most recent incidence of a in l appears after b
+        last_index_a = max([i for i, x in enumerate(l) if x == a], default=-1)
+        last_index_b = l.index(b) if b in l else -1
+
+        return last_index_a > last_index_b
 
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+    global recorded_inputs
+    print(key)
+    if already_pressed(recorded_inputs, (key, 'down'), (key, 'up')):
+        print('already pressed')
+        pass
+    else:
+        print('not already pressed')
+        recorded_inputs.append((key, 'down'))
 
 def on_release(key):
-    print('{0} released'.format(
-        key))
+    global recorded_inputs
+    print(key)
+    recorded_inputs.append((key, 'up'))
     if key == keyboard.Key.esc:
-        running = False
+        # Stop listener
+        print(recorded_inputs)
         return False
 
-listener = keyboard.Listener(
-    on_press=on_press, on_release=on_release)
-listener.start()
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release, suppress=True) as listener:
+    listener.join()
 
-print("\nPress arrows to adjust, enter or esc to quit\n")
-
-def main():
-    while running:
-        pass
-
-
-main()
+listener.stop()
